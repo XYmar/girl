5dc17cbfa9c24c66dce77e6fd8459ce45c039ea1
XYmar <xqingliuwu@163.com>
2018-03-28 17:05:02 +0800

    通过yml文件配置女生的属性，可以配置单个的，也可以组合配置。

--- src/main/java/com/xy/girl/HelloController.java
+++ src/main/java/com/xy/girl/HelloController.java
@@ -1,5 +1,6 @@
 package com.xy.girl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${prettyLevel}")
+    private String prettyLevel;      //通过注解把配置文件中的值注入到这个变量里了
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String say(){
-        return "Hello Spring Boot!";
+        return content;
+        //return prettyLevel + age;
+        //return "Hello Spring Boot!";
     }
 }

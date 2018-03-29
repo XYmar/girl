8b30046392f9f8f3df447f04bbb1c1873f988ce6
XYmar <xqingliuwu@163.com>
2018-03-29 16:41:26 +0800

    实现对数据库表信息的增删改查操作

---
+++ src/main/java/com/xy/girl/GirlController.java
@@ -0,0 +1,75 @@
+package com.xy.girl;
+
+import org.omg.PortableInterceptor.INACTIVE;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+@RestController
+public class GirlController {
+
+    @Autowired
+    private GirlRepository girlRepository;
+
+    /**
+     * 查询所有女生列表
+     * */
+    @GetMapping(value = "/girls")
+    public List<Girl> girlList(){
+        return girlRepository.findAll();
+    }
+
+
+    /**
+     * 添加一个女生
+     * */
+    @PostMapping(value = "/girls")
+    public Girl girlAdd(@RequestParam("prettyLevel") String prettyLevel,
+                          @RequestParam("age") Integer age){
+        Girl girl = new Girl();
+        girl.setPrettyLevel(prettyLevel);
+        girl.setAge(age);
+        return girlRepository.save(girl);
+    }
+
+    /*
+     * 查询一个女生
+     * */
+    @GetMapping(value = "girls/{id}")
+    public Girl girlFindOne(@PathVariable("id") Integer id){
+        return girlRepository.findById(id).get();
+    }
+
+    /*
+    * 更新一个女生信息
+    * */
+    @PutMapping(value = "girls/{id}")
+    public Girl girlUpdate(@PathVariable("id") Integer id,
+                           @RequestParam("prettyLevel") String prettyLevel,
+                           @RequestParam("age") Integer age){
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setPrettyLevel(prettyLevel);
+        girl.setAge(age);
+
+        return girlRepository.save(girl);
+    }
+
+    /*
+    * 删除一个女生
+    * */
+    @DeleteMapping(value = "girls/{id}")
+    public void girlDelete(@PathVariable("id") Integer id){
+        girlRepository.deleteById(id);
+    }
+
+    /*
+    * 根据年龄来查询女生信息
+    * */
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> girlByAge(@PathVariable("age") Integer age){
+        return girlRepository.findByAge(age);
+    }
+}
